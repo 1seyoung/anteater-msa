d64a23e3740605c9dec29dee473985ba7e571943
1seyoung <20171631@edu.hanbat.ac.kr>
2024-08-07 23:45:56 +0900

    first commit

---
+++ src/main/java/com/anteater/stocktalk/StocktalkApplication.java
@@ -0,0 +1,13 @@
+package com.anteater.stocktalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StocktalkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StocktalkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/anteater/stocktalk/StocktalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anteater.stocktalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StocktalkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
