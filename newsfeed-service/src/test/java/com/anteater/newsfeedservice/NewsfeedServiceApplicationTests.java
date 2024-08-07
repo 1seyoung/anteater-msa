eccb68393099009dea77774540eec3c0791b21a2
1seyoung <20171631@edu.hanbat.ac.kr>
2024-08-08 00:08:51 +0900

    init commit

---
+++ newsfeed-service/src/test/java/com/anteater/newsfeedservice/NewsfeedServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anteater.newsfeedservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsfeedServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
